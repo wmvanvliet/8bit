be9c0ee5d8776cda78ed34f49f0a03912979334b
Marijn van Vliet <w.m.vanvliet@gmail.com>
2022-04-06 17:06:33 +0300

    Better version of divide.asm

--- example_programs/divide.asm
+++ example_programs/divide.asm
@@ -1,21 +1,21 @@
-; Divides two positive numbers, also computes the remainder.
-; Works in signed integer modes, so numbers need to be <= 127.
+; Divides two positive numbers.
 loop:	lda numer
-	sta remain
 	sub denom
+	jc inc     ; result was non-negative
+	jmp end
+
+inc:
 	sta numer
-	add check  ; check the sign bit of the result
-	jc end     ; result was negative
 	lda ans
 	add one
 	sta ans
 	jmp loop
+
 end:    lda ans
-        out
+	out
 	hlt
+
 one:    db 1
-check:	db 64  ; checks for the sign bit
-numer:	db 35 
+numer:	db 8 
 denom:	db 8
 ans:	db 0
-remain: db 0
