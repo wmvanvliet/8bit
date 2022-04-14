76625f7af872937f500d703a553ff4d42876e353
Marijn van Vliet <w.m.vanvliet@gmail.com>
2022-04-14 16:41:31 +0300

    Add comments

--- example_programs/multiply_shift.asm
+++ example_programs/multiply_shift.asm
@@ -1,19 +1,21 @@
 ;
 ; Multiply two numbers (x and y) using left-shift
 ;
-loop:	lda y
-	add y
-	sta y
-	lda prod
-	jc a
-	jmp b
-a:	add x
-	out
-b:	sta prod
-        add prod
-	sta prod
-	jmp loop
+loop:	lda y            ; Step 1: look at the most-significant bit of y
+	add y            ; Left-shift y, sets the carry flag if MSB was a 1
+	sta y            ; Store y with the MSB removed
+	lda prod         ; Load intermediate result (the product)
+	jc add_x         ; If the MSB was a 1, add x to the intermediate result
+	jmp shift_result ; Else skip over the adding part
+add_x:	add x            ; Add x to the intermediate result
+	out              ; Output our intermediate result
+	
+shift_result:
+	sta prod         ; Left-shift intermediate result
+        add prod         ; 
+	sta prod         ;
+	jmp loop         ; Next iteration
 
-x:	db 17
-y:	db 15
-prod:	db 0
+x:	db 17            ; We are computing 17 x 15
+y:	db 15            ;
+prod:	db 0             ; The (intermediate) result is stored here
